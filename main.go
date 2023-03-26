d64741a114954f393926efb4ebb1d69732f58f59
Viky <vigneshvics13@gmail.com>
2023-03-26 16:21:50 +0530

    changes

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
@@ -9,15 +10,22 @@ import (
 
 func main(){
 	app := fiber.New()
-	http.HandleFunc("/", Hello)
-	http.HandleFunc("/welcome",welcome)
-	http.ListenAndServe(":8080",nil)
+	app.Get("/",func(c *fiber.Ctx) error {
+		return c.SendString("Hello")
+	})
+
+	app.Get("/env",func(c *fiber.Ctx) error {
+		return c.SendString("env")
+	})
+	port :="3000"
+	log.Fatal(app.Listen("0.0.0.0:"+port))
+	
 }
 
-func Hello(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w,"Hello")
-}
+// func Hello(w http.ResponseWriter, r *http.Request){
+// 	fmt.Fprintln(w,"Hello")
+// }
 
-func welcome(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w,"welcome")
-}
+// func welcome(w http.ResponseWriter, r *http.Request){
+// 	fmt.Fprintln(w,"welcome")
+// }
